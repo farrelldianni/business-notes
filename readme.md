8902719e6aa4a8889857ccb95339db3b9e9f6c78
frl <103608522+farrelldianni@users.noreply.github.com>
2022-08-18 21:02:23 -0400

    last commit, heroku being weird

--- readme.md
+++ readme.md
@@ -7,3 +7,20 @@ AS A small business owner
 I WANT to be able to write and save notes
 SO THAT I can organize my thoughts and keep track of tasks I need to complete
 </p>
+
+<h2>Getting Up & Running</h2>
+<p>
+The application has a db.json file on the back end that is used to store and retrieve notes using the fs module.
+
+The following HTML routes was created:
+<ul>
+    <li>GET /notes returns the notes.html file.</li>
+    <li>GET * returns the index.html file.</li>
+</ul>
+
+The following API routes was created:
+<ul>
+<li>GET /api/notes reads the db.json file and returns all saved notes as JSON.</li>
+<li>POST /api/notes receives a new note to save on the request body, adds it to the db.json file, and then returns the new note to the client. Each note has a unique ID.</li>
+</ul>
+</p>
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+//uuid
+const { v4: uuidv4 } = require('uuid');
+
+//red notes from db.json
+let notes = fs.readFileSync('./db/db.json');
+notes = JSON.parse(notes)
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+//home route
+app.get('/', (req,res)=> res.sendFile(path.join(__dirname, '/public/index.html')));
+
+//notes route 
+app.get('/notes', (req,res)=> res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+//api view
+app.get('/api/notes', (req, res)=> res.json(notes));
+
+//post
+app.post('/api/notes', (req, res) => {
+
+    //add object 
+    const newN = req.body 
+    newN.id = uuidv4();
+
+    //log data in console
+    console.log("Adding Note: ", newN);
+
+    //add data 
+    notes.push(newN);
+
+    //write files to db
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) =>
+    err ? res.send( err ) : res.json(notes)
+    );
+
+});
+
+//delete note
+app.delete('/api/notes/:id', (req, res) => {
+
+    const selectedNoteID = req.params.id;
+    console.log(`Removing item with id: ${selectedNoteID}`);
+
+    
+    notes = notes.filter(note => note.id != selectedNoteID);
+
+    res.end();
+});
+
+
+app.listen(PORT, () => console.log(`server started on http://localhost:${PORT}`));
